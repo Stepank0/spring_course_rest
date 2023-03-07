e412ca4d0bc9e37b98d6322d21faef80e1d0bcb3
Stepan Koryagin <stepanswim@gmail.com>
2023-03-07 23:02:56 +0200

    add ExceptionHandler

--- src/main/java/com/zaurtregulov/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/zaurtregulov/spring/rest/controller/MyRESTController.java
@@ -1,11 +1,13 @@
 package com.zaurtregulov.spring.rest.controller;
 
 import com.zaurtregulov.spring.rest.entity.Employee;
+import com.zaurtregulov.spring.rest.exception_handling.EmployeeIncorrectData;
+import com.zaurtregulov.spring.rest.exception_handling.NoSuchEmployeeException;
 import com.zaurtregulov.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,28 +18,38 @@ public class MyRESTController {
     private EmployeeService employeeService;
 
     @GetMapping("/employees")  //тоже самое что RequestMapping только специализированно под GEТ запрос
-    public List<Employee> showAllEmployees(){
-
+    public List<Employee> showAllEmployees() {
         List<Employee> allEmployees = employeeService.getAllEmployees();
+
         return allEmployees;
     }
 
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable int id) {
+        // @PathVariable int id - используется для  получения значения переменной из адреса запроса
+        Employee employee = employeeService.getEmployee(id);
 
+        if(employee == null){
+            throw new NoSuchEmployeeException("There is no employee with ID = " +
+                    id + " int Database");
+        }
 
+        return employee;
+    }
+    @ExceptionHandler   // отмечается метод , ответственный за обработку исключений
+    public ResponseEntity<EmployeeIncorrectData> handleException(NoSuchEmployeeException exception){
+        EmployeeIncorrectData data = new EmployeeIncorrectData();
+        data.setInfo(exception.getMessage());
 
+        return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
+    }
 
+    @ExceptionHandler   // отмечается метод , ответственный за обработку исключений
+    public ResponseEntity<EmployeeIncorrectData> handleException(Exception exception){
+        EmployeeIncorrectData data = new EmployeeIncorrectData();
+        data.setInfo(exception.getMessage());
 
-
-
-
-
-
-
-
-
-
-
-
-
+        return new ResponseEntity<>(data, HttpStatus.BAD_REQUEST);
+    }
 
 }
